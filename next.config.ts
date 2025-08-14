6bbca30494289c697d52ce53d42c6274865becae
Niclic2 <nicolasniclic@gmail.com>
2025-08-14 14:04:33 +0400

    committin manifest

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+
+  // basePath: Должен быть только путь к репозиторию, начинающийся со слэша.
+  // Без домена, без протокола.
+  basePath: process.env.NODE_ENV === 'production' ? '/freemyth' : '',
+
+  // assetPrefix: Должен быть полный, валидный URL, включая протокол и завершающий слэш.
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://niclic2.github.io/freemyth/' : '',
+
+  images: {
+    unoptimized: true, // Важно, если используете next/image
+  },
 };
 
 export default nextConfig;
